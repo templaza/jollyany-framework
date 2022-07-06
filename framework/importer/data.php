a838cb86b12d2321dec09a94099b77a89f87fe34
Sonny <sonvnn@gmail.com>
2022-07-06 08:45:58 +0700

    Update v3.5.8

--- framework/importer/data.php
+++ framework/importer/data.php
@@ -94,6 +94,34 @@ class JollyanyFrameworkDataImport {
 
 	public static function getData() {
 	    self::$data   =   array(
+            'tz_agruco'      =>  array(
+                // Pack Info
+                'name'        => 'Agruco',
+                'desc'        => 'Agriculture & Organic Food Joomla Template',
+
+                // Pack Data
+                'thumb'       => '/images/stories/agruco/thumbnail.jpg',
+                'category'    => 'joomla',
+
+                'demo_url'    => 'https://agruco.jollyany.co/',
+                'doc_url'     => 'https://docs.jollyany.co/templates/agruco',
+
+                'joomla_version'     => array(3,4),
+
+                'template'      => array(
+                    'name'      =>  'Agruco Template',
+                    'type'      =>  'included',
+                    'code'      =>  'tz_agruco',
+                    'ext_code'  =>  'tz-agruco-api',
+                ),
+
+                'extensions'  => array(
+                    self::$exts['sp-page-builder'],
+                    self::$exts['uk-sp-addons'],
+                    self::$exts['tz-portfolio'],
+                    self::$exts['hikashop'],
+                ),
+            ),
             'tz_oraz'      =>  array(
                 // Pack Info
                 'name'        => 'Oraz',
