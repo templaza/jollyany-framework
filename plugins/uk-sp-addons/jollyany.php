35301d1bd5ccded6479ef16f0aa6f6e76468288b
Sonny Le <sonlv@templaza.com>
2024-02-22 14:31:33 +0700

    Add UK Addon installation

---
+++ plugins/uk-sp-addons/jollyany.php
@@ -0,0 +1,14 @@
+<?php
+/**
+* @package SP Page Builder Jollyany Add-Ons
+* @author TemPlaza http://www.templaza.com
+* @copyright Copyright (c) 2011 - 2021 TemPlaza
+* @license http://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 or later
+*/
+
+//no direct accees
+defined('_JEXEC') or die('Restricted Aceess');
+
+class PlgSppagebuilderJollyany extends JPlugin {
+	protected $autoloadLanguage = true;
+}
